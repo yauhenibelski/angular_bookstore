0d06aaaea3958810f0c7d999b8ce10f0a8fc0427
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-04-30 22:31:18 +0200

    chore: add prettier eslint airbnb style guide

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,19 +11,23 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it(`should have the 'final-task' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('final-task');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
+
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
+
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, final-task');
   });
 });
