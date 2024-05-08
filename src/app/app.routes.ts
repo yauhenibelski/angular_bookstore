1eaf0cd032f31a95c1679d8fe1071cd402b906f3
Kristina Homenkova <kristina.khomenkova@gmail.com>
2024-05-08 15:12:35 +0300

    feat: added login and books pages, added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./pages/books/books-page.component').then(mod => mod.BooksPageComponent),
+    },
+    {
+        path: 'registration',
+        loadComponent: () =>
+            import('./pages/registration/registration-page.component').then(
+                mod => mod.RegistrationPageComponent,
+            ),
+    },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./pages/login/login-page.component').then(mod => mod.LoginPageComponent),
+    },
+    {
+        path: '**',
+        loadComponent: () =>
+            import('./pages/404/not-found-page.component').then(mod => mod.NotFoundPageComponent),
+    },
+];
