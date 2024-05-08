5d47dcfe0a0ddcb461caa7395ccf48c276eb19b7
Kristina Khomenkova <128573385+kris-home@users.noreply.github.com>
2024-05-08 14:36:54 +0300

    RSS-ECOMM-2_20: Implement a 404 (Not Found) page for invalid route requests (#10)
    * feat: added 404 page
    
    * fix: corrections based on reviewer comments

---
+++ src/app/pages/404/not-found-page.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+    selector: 'app-not-found-page',
+    standalone: true,
+    imports: [MatButton],
+    templateUrl: './not-found-page.component.html',
+    styleUrl: './not-found-page.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NotFoundPageComponent {
+    returnWindowHistoryBack(): void {
+        window.history.back();
+    }
+}
