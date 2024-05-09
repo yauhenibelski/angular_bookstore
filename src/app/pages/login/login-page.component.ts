dd47c8262b10ad8de769c9dc88aba6643a7cabbb
Kristina Homenkova <kristina.khomenkova@gmail.com>
2024-05-09 15:48:53 +0300

    feat: added login form

--- src/app/pages/login/login-page.component.ts
+++ src/app/pages/login/login-page.component.ts
@@ -1,11 +1,35 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { isEmail } from './validators/email';
+import { GetErrorMassagePipe } from './pipes/get-error-massage.pipe';
+import { passwordValidators } from './validators/password';
 
 @Component({
     selector: 'app-login-page',
     standalone: true,
-    imports: [],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatCardModule,
+        MatInputModule,
+        MatButtonModule,
+        GetErrorMassagePipe,
+        MatIconModule,
+    ],
     templateUrl: './login-page.component.html',
     styleUrl: './login-page.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+    isPasswordHide = true;
+    readonly loginForm = new FormGroup({
+        email: new FormControl('', [isEmail]),
+        password: new FormControl('', [...Object.values(passwordValidators)]),
+    });
+
+    readonly controls = this.loginForm.controls;
+}
---
+++ src/app/pages/login/pipes/get-error-massage.pipe.spec.ts
@@ -0,0 +1,9 @@
+import { GetErrorMassagePipe } from './get-error-massage.pipe';
+
+describe('GetErrorMassagePipe', () => {
+    it('create an instance', () => {
+        const pipe = new GetErrorMassagePipe();
+
+        expect(pipe).toBeTruthy();
+    });
+});
