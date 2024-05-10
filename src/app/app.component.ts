c6dd22d0c1c45b8a326bd7b926eef2a093d03684
Kristina Khomenkova <128573385+kris-home@users.noreply.github.com>
2024-05-10 21:50:46 +0300

    RSS-ECOMM-2_01: Implement client-side validation for login form (#15)
    * feat: added login form
    
    * refactor: login form
    
    * feat: added an authorization form, added a footer and general background settings
    
    * fix: conflict fixed
    
    * fix: corrections based on reviewer comments
    
    * fix: corrections based on reviewer comments
    
    ---------
    
    Co-authored-by: yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RegistrationPageComponent } from './pages/registration/registration-page.component';
 import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, RegistrationPageComponent, HeaderComponent],
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        RegistrationPageComponent,
+        HeaderComponent,
+        FooterComponent,
+    ],
 })
 export class AppComponent {
     title = 'final-task';
--- src/app/pages/login/login-page.component.ts
+++ src/app/pages/login/login-page.component.ts
@@ -1,11 +1,37 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { isEmail } from '../../shared/form-validators/email';
+import { GetErrorMassagePipe } from '../../shared/pipes/get-error-massage/get-error-massage.pipe';
+import { passwordValidators } from '../../shared/form-validators/password';
 
 @Component({
     selector: 'app-login-page',
     standalone: true,
-    imports: [],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatCardModule,
+        MatInputModule,
+        MatButtonModule,
+        GetErrorMassagePipe,
+        MatIconModule,
+        RouterLink,
+    ],
     templateUrl: './login-page.component.html',
     styleUrl: './login-page.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+    isPasswordHide = true;
+    readonly loginForm = new FormGroup({
+        email: new FormControl('', [isEmail]),
+        password: new FormControl('', [...Object.values(passwordValidators)]),
+    });
+
+    readonly controls = this.loginForm.controls;
+}
