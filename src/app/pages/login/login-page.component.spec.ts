5c2c8131a3b87315cac09f068889788e97de7636
Lena <108792923+EllenLis@users.noreply.github.com>
2024-05-20 13:11:22 +0300

    feat: add unit tests for project (FTD-29) (#33)

--- src/app/pages/login/login-page.component.spec.ts
+++ src/app/pages/login/login-page.component.spec.ts
@@ -1,22 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ApiService } from '../../shared/services/api/api.service';
+import { AuthService } from '../../shared/services/auth/auth.service';
 import { LoginPageComponent } from './login-page.component';
 
 describe('LoginPageComponent', () => {
-    let component: LoginPageComponent;
-    let fixture: ComponentFixture<LoginPageComponent>;
-
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [LoginPageComponent],
+            imports: [LoginPageComponent, HttpClientTestingModule, RouterTestingModule],
+            providers: [ApiService, AuthService, { provide: MatBottomSheet, useValue: {} }],
+            schemas: [NO_ERRORS_SCHEMA],
         }).compileComponents();
-
-        fixture = TestBed.createComponent(LoginPageComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
     });
 
     it('should create', () => {
+        const fixture = TestBed.createComponent(LoginPageComponent);
+        const component = fixture.componentInstance;
+
         expect(component).toBeTruthy();
     });
 });
