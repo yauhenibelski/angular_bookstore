152b4b2d0954290d3f03df775d067c7c46ddd526
Kristina Homenkova <kristina.khomenkova@gmail.com>
2024-06-03 20:53:44 +0300

    fix: fix

--- src/app/pages/books/category/category.component.ts
+++ src/app/pages/books/category/category.component.ts
@@ -2,12 +2,24 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { AsyncPipe } from '@angular/common';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 import { CategoryService } from './service/category.service';
 
 @Component({
     selector: 'app-category',
     standalone: true,
-    imports: [MatListModule, AsyncPipe, RouterLink],
+    imports: [
+        MatListModule,
+        AsyncPipe,
+        RouterLink,
+        MatMenuModule,
+        MatIconModule,
+        MatButtonModule,
+        MatSidenavModule,
+    ],
     templateUrl: './category.component.html',
     styleUrl: './category.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/pages/books/search/search.component.ts
+++ src/app/pages/books/search/search.component.ts
@@ -6,11 +6,19 @@ import { RouterLink } from '@angular/router';
 import { Observable, debounceTime, distinctUntilChanged, filter, map, switchMap } from 'rxjs';
 import { Product } from 'src/app/interfaces/product';
 import { ApiService } from 'src/app/shared/services/api/api.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-search',
     standalone: true,
-    imports: [FormsModule, MatAutocompleteModule, ReactiveFormsModule, AsyncPipe, RouterLink],
+    imports: [
+        FormsModule,
+        MatAutocompleteModule,
+        ReactiveFormsModule,
+        AsyncPipe,
+        RouterLink,
+        MatIconModule,
+    ],
     templateUrl: './search.component.html',
     styleUrl: './search.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
