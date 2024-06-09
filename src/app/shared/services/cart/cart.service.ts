56a4f0349d54be845e2f17b1231644e3b0dea71f
Yauheni Belski <108809026+yauhenibelski@users.noreply.github.com>
2024-06-09 15:30:10 +0200

    feat: add product to cart functionality (#57)

--- src/app/shared/services/cart/cart.service.ts
+++ src/app/shared/services/cart/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Cart } from './cart.interface';
 
 @Injectable({
@@ -8,15 +9,51 @@ import { Cart } from './cart.interface';
 export class CartService {
     private readonly cartSubject = new BehaviorSubject<Cart | null>(null);
 
+    constructor(private readonly httpClient: HttpClient) {}
+
     get cart$() {
         return this.cartSubject.asObservable();
+    }
+
+    get cart() {
+        return this.cartSubject.value;
+    }
+
+    private get cartId() {
+        return `${this.cartSubject.value?.id}`;
     }
 
     setCart(cart: Cart | null): void {
         this.cartSubject.next(cart);
     }
 
-    get cart() {
-        return this.cartSubject.value;
+    hasProductInCart(productId: string): boolean {
+        return Boolean(this.cart?.lineItems.find(product => product.id === productId));
+    }
+
+    private addToCartSubscription: Subscription | null = null;
+
+    addToCart(productId: string): void {
+        if (this.addToCartSubscription) {
+            this.addToCartSubscription.unsubscribe();
+        }
+
+        const payload = {
+            action: 'addLineItem',
+            productId,
+            quantity: 1,
+        };
+
+        this.addToCartSubscription = this.httpClient
+            .post<Cart>(`/carts/${this.cartId}`, {
+                version: this.cart?.version,
+                actions: [payload],
+            })
+            .subscribe({
+                next: cart => this.cartSubject.next(cart),
+                complete: () => {
+                    this.addToCartSubscription = null;
+                },
+            });
     }
 }
