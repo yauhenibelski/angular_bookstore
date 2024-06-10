feeb84f0f2dce4eb17676ebcf4c217e7f6e7a9ad
Kristina Khomenkova <128573385+kris-home@users.noreply.github.com>
2024-06-10 16:28:44 +0300

    feat: added 'about us' component, added a button to go to the about us page (#61)

---
+++ src/app/pages/about-us/about-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-about-page',
+    standalone: true,
+    imports: [],
+    templateUrl: './about-page.component.html',
+    styleUrl: './about-page.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutPageComponent {}
